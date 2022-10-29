fe12282ba448561fef80e2bdacdc10f158d5ff6c
vc174887 <vc174887@fh-muenster.de>
2022-10-29 16:44:46 +0200

    bad version, problem with async ecmascript 6 callback  event loop

--- src/index.js
+++ src/index.js
@@ -1,61 +1,47 @@
+console.log("##### Starting Demo #####");
 import Kurs from './models/kurs.js';
 import Hochschule from './models/hochschule.js';
 import Professor from './models/professor.js';
 import Student from './models/student.js';
-console.log("##### Starting Demo #####");
+import startDB from './mongo_access/db.js';
 
+//Models erzeugen
 var fh_muenster = new Hochschule("FA Münster");
-var student1 = new Student("tim", 22, fh_muenster, "1234");
-var student2 = new Student("simon", 21, fh_muenster, "1235");
-var student3 = new Student("kevin", 23, fh_muenster, "1236");
-var student4 = new Student("pascal", 22, fh_muenster, "1237");
+var studenten = [
+    new Student("tim", 22, fh_muenster, "1234"),
+    new Student("simon", 21, fh_muenster, "1235"),
+    new Student("kevin", 23, fh_muenster, "1236"),
+    new Student("pascal", 22, fh_muenster, "1237")
+];
 var professor = new Professor("Humernbrum", 30, fh_muenster);
-var course = new Kurs(
-    "BigData", [
-        student1,
-        student2,
-        student3,
-        student4,
-    ],
+var kurs = new Kurs(
+    "BigData",
+    studenten,
     professor,
     fh_muenster,
 );
 
-console.log(course.toString());
-
-
-
-
-
-//CRUD
-//Create
-//await runInsertion(collection);
-//Read
-//await runSelection(collection);
-//Update
-//Delete
-
-//Hier geben studierende ihren code ein, um Aufgabe zu lösen
-async function runInsertion(collection) {
-    let employee1 = new Person("tim", 18);
-    let employee2 = new Person("kevin" , 19);
-    await collection.insertOne(employee1, (err, result) => {
-        if (!err) { console.log('Inserted Document'); }
-    });
-    await collection.insertOne(employee2, (err, result) => {
-        if (!err) { console.log('Inserted Document'); }
-    });
-}
-
-
-//Hier geben studierende ihren code ein, um Aufgabe zu lösen
-function runSelection(collection) {
-     collection.find({ age: 18 })
-    .sort({ name: 1 })
-    .limit(2)
-    .toArray((err, resultSet) => {
-    if (!err) { 
-        console.log(resultSet);
+startDB(
+    async function (db) {
+        var personCollection = db.collection("person");
+        var hochschuleCollection = db.collection("hochschule");
+        var kursCollection = db.collection("kurs");
+        await insertPersonen(personCollection);
+        //await selectPersonen(personCollection);
+        //personCollection.drop();
     }
+);
+
+//Insert Data to DataBase
+async function insertPersonen(personCollection) {
+    //Save Studenten
+    const result = await personCollection.insertMany()
+    //Insert Professor
+}
+//Select Data from DataBase
+async function selectPersonen(personCollection) {
+    personCollection.find()
+    .toArray((err, resultSet) => {
+        if(!err) console.log(resultSet);
     });
 }
--- src/mongo_access/db.js
+++ src/mongo_access/db.js
@@ -1,34 +1,26 @@
 import MongoDB from "mongodb";
 
-const userName = 'user';
-const password = 'mypass';
-const databaseName = 'mydatabase';
-const url = 'mongodb://localhost:27017';
-const options = { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    auth: { 
-        username: userName, 
-        password: password 
-    }, 
-    authSource: databaseName
-};
-let personCollection = null;
-let hochschuleCollection = null;
-let studentCollection = null;
-let professorCollection = null;
-let courseCollection = null;
-
-MongoDB.MongoClient.connect(url, options, async (err, client) => {
-    if (err) {
-        console.log('Could not connect to MongoDB: ', err.stack);
-        process.exit(1);
-    } else {
-        var db = client.db(databaseName);
-        personCollection = db.collection('person');
-        //hochschuleCollection = db.collection('hochschule');
-    }
-});
-export {
-    personCollection
-};
+export default function startDB(callback) {
+    const userName = 'user';
+    const password = 'mypass';
+    const databaseName = 'mydatabase';
+    const url = 'mongodb://localhost:27017';
+    const options = {   
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        auth: { 
+            username: userName, 
+            password: password 
+        }, 
+        authSource: databaseName
+    };
+    MongoDB.MongoClient.connect(url, options, async (err, client) => {
+        if (err) {
+            console.log('Could not connect to MongoDB: ', err.stack);
+            process.exit(1);
+        } else {
+            var db = client.db(databaseName);
+            await callback(db);
+        }
+    });
+}
